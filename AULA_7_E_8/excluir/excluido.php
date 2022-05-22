052779bc760397168b1cdd22c53d11a5c8bec211
beriba-faeterj <104885703+beriba-faeterj@users.noreply.github.com>
2022-05-22 19:52:45 -0300

    Add files via upload

---
+++ AULA_7_E_8/excluir/excluido.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+
+Nome: Lucas Beriba
+Matrícula: 2110478300001
+Data: 12/05/2022 e 19/05/2022
+
+Tema da aula: Exercício de fixação para a prova
+
+-->
+
+<head>
+	<title>Listar todos</title>
+    <link rel="stylesheet" href="../formatacao.css">
+</head>
+<body>
+    <header>
+        Trabalho aula 7 12/05/2022 e aula 8 19/05/2022 Beriba
+    </header>
+    <nav class="flex-container">
+        <div><a href="../index.php">Início</a></div>
+        <div><a href="../incluir/incluir.php">Incluir</a></div>
+        <div><a href="../alterar/alterar.php">Alterar</a></div>
+        <div><a href="../listar/listar_todos.php">Listar todos</a></div>
+        <div><a href="../listar/listar_um.php">Listar um</a></div>
+        <div><a href="excluir.php">Excluir</a></div>
+    </nav>
+    <main>
+    <?php
+
+        include '../biblioteca_projeto.php';
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (!file_exists($arquivoProjeto)) {
+                echo "Arquivo não existe.";
+            }
+            else {
+                $lista = vetorizaArquivo($arquivoProjeto);
+
+                if (isset($_POST["excluirID"])) {
+                    $pos = encontraNaPosicaoX($lista, $_POST["id"], $posID);
+                }
+                else {
+                    $pos = encontraNaPosicaoX($lista, $_POST["codBarras"], $posCodBarras);
+                }
+
+                if ($pos == -1) {
+                    echo "Produto não encontrado.";
+                }
+                else {
+                    $arquivo = fopen($arquivoProjeto, "w");
+                    fwrite($arquivo, $stringCabecalho . "\n");
+
+                    $qtdItens = retornaUltimoIndice($lista);
+
+                    for ($i = 0; $i < $qtdItens; $i++) {
+                        if ($i == $pos) {
+                            echo "Produto excluído. ";
+
+                            if (isset($_POST["excluirID"]))
+                                echo "(ID: " . $_POST["id"] . ")";
+                            else
+                                echo "(COD. BARRAS: " . $_POST["codBarras"] . ")";
+
+                            continue;
+                        }
+
+                        $linha = "";
+
+                        for ($j = 0; $j < $qtdColunasCabecalho; $j++) {
+                            $linha = $linha . $lista[$i][$j];
+
+                            if ($j < ($qtdColunasCabecalho-1)) {
+                                // Se não estiver na última posição, coloque o ponto e vírgula
+                                // (O último item já possui uma quebra de linha atrelada)
+                                $linha = $linha . ";";
+                            }
+                        }
+
+                        fwrite($arquivo, $linha);
+                    }
+                    
+                    fclose($arquivo);
+                }
+            }
+        }
+
+        ?>
+    <main>
+</body>
+</html>
---
+++ AULA_7_E_8/listar/listar_todos.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+
+Nome: Lucas Beriba
+Matrícula: 2110478300001
+Data: 12/05/2022 e 19/05/2022
+
+Tema da aula: Exercício de fixação para a prova
+
+-->
+
+<head>
+	<title>Listar todos</title>
+    <link rel="stylesheet" href="../formatacao.css">
+</head>
+<body>
+    <header>
+        Trabalho aula 7 12/05/2022 e aula 8 19/05/2022 Beriba
+    </header>
+    <nav class="flex-container">
+        <div><a href="../index.php">Início</a></div>
+        <div><a href="../incluir/incluir.php">Incluir</a></div>
+        <div><a href="../alterar/alterar.php">Alterar</a></div>
+        <div><a href="">Listar todos</a></div>
+        <div><a href="listar_um.php">Listar um</a></div>
+        <div><a href="../excluir/excluir.php">Excluir</a></div>
+    </nav>
+    <main>
+        <?php 
+
+            include '../biblioteca_projeto.php';
+
+            if (!file_exists($arquivoProjeto)) {
+                echo "Arquivo não existe.";
+            }
+            else {
+                $lista = vetorizaArquivo($arquivoProjeto);
+    
+                // Ajusta a quantidade (sizeof não começa a contar do 0 como o vetor)
+                $qtdItens = retornaUltimoIndice($lista);
+    
+                for ($i = 0; $i < $qtdItens; $i++) {
+                    for ($j = 0; $j < $qtdColunasCabecalho; $j++) {
+                        echo $arrayCabecalho[$j] . ": " . $lista[$i][$j] . "<br>" ;
+                    }
+
+                    echo '<form method="post" action="../excluir/excluido.php">
+                    <input type="hidden" name="id" value="' . $lista[$i][$posID]. '">
+                    <input type="submit" name="excluirID" value="Excluir">
+                    </form>';
+    
+                    echo "<br><br>";
+                }
+            }
+        ?>
+    <main>
+</body>
+</html>
