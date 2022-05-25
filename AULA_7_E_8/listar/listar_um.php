c550986b3fc299feee7dd6e2dc9d7a27918e7bb7
beriba-faeterj <104885703+beriba-faeterj@users.noreply.github.com>
2022-05-25 08:44:14 -0300

    Add files via upload

---
+++ AULA_7_E_8/listar/listar_um.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+
+Nome: Lucas Beriba
+Matrícula: 2110478300001
+Data: 12/05/2022 e 19/05/2022
+
+Tema da aula: Exercício de fixação para a prova
+
+-->
+
+<head>
+	<title>Listar todos</title>
+    <link rel="stylesheet" href="../formatacao.css">
+</head>
+<body>
+    <header>
+        Trabalho aula 7 12/05/2022 e aula 8 19/05/2022 Beriba
+    </header>
+    <nav class="flex-container">
+        <div><a href="../index.php">Início</a></div>
+        <div><a href="../incluir/incluir.php">Incluir</a></div>
+        <div><a href="../alterar/alterar.php">Alterar</a></div>
+        <div><a href="listar_todos.php">Listar todos</a></div>
+        <div><a href="">Listar um</a></div>
+        <div><a href="../excluir/excluir.php">Excluir</a></div>
+    </nav>
+    <main>
+        <form method="post" action="listado.php">
+            Procurar por ID: <input type="text" name="id" value="">
+            <input type="submit" name="procurarID" value="Enviar">
+        </form>
+        <br>
+        <form method="post" action="listado.php">
+            Pocurar pelo código de barras: <input type="text" name="codBarras" value="">
+            <input type="submit" name="procurarCodBarras" value="Enviar">
+        </form>
+    <main>
+</body>
+</html>
---
+++ AULA_7_E_8/test/test1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+
+<!--
+
+Nome: Lucas Beriba
+Matrícula: 2110478300001
+Data: 12/05/2022 e 19/05/2022
+
+Tema da aula: Exercício de fixação para a prova
+
+-->
+
+<head>
+	<title>Incluir</title>
+    <link rel="stylesheet" href="../formatacao.css">
+</head>
+<body> 
+    <header> 
+        Trabalho aula 7 12/05/2022 e aula 8 19/05/2022 Beriba
+    </header>
+    <nav class="flex-container">
+        <div><a href="../index.php">Início</a></div>
+        <div><a href="">Incluir</a></div>
+        <div><a href="../alterar/alterar.php">Alterar</a></div>
+        <div><a href="../listar/listar_todos.php">Listar todos</a></div>
+        <div><a href="">Listar um</a></div>
+        <div><a href="">Excluir</a></div>
+    </nav>
+    <main>
+
+<?php
+
+include '../biblioteca_projeto.php';
+
+print_r(preg_match('/\d+(?:[,]\d{1,2})?/', 'a'));
+
+print_r('\n');
+
+print_r(0 || 0);
+
+if (validaString('9', '/\d+(?:[,]\d{1,2})?/') == true) {
+    print_r('true');
+}
+else print_r('false');
+
+// url: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|jpeg|png)/
+
+// fazer 1 funcao que recebe a string com o padrao e usar regex pra tudo?
+
+?>
+
+        <form method="post" action="test2.php">
+            <button type="submit" value="aaa" name="aaa">butao aaa</button>
+        </form>
+        <form method="post" action="test2.php">
+            <button type="submit" value="bbb" name="bbb">butao bbb</button>
+        </form>
+    <main>
+</body>
+</html>
+
+
