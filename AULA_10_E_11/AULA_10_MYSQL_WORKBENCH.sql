bf8c87aa96c2c169cb4ab4853339d1caa61658e8
beriba-faeterj <104885703+beriba-faeterj@users.noreply.github.com>
2022-06-09 09:52:45 -0300

    Add files via upload

---
+++ AULA_10_E_11/AULA_10_MYSQL_WORKBENCH.sql
@@ -0,0 +1,18 @@
+
+
+CREATE TABLE `usuario` (
+	`id` INT NOT NULL AUTO_INCREMENT ,
+	`nome` VARCHAR(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+	`matricula` VARCHAR(16) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+	`funcao` VARCHAR(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+	`senha` VARCHAR(100) CHARACTER SET utf16 COLLATE utf16_unicode_ci NOT NULL,
+	PRIMARY KEY (`id`), UNIQUE (`matricula`)
+) ENGINE = MyISAM;
+	
+INSERT INTO `usuario`(`id`, `nome`, `matricula`, `funcao`, `senha`)
+VALUES (10,'teste','111','aaa','')
+
+INSERT INTO `usuario`(`nome`, `matricula`, `funcao`, `senha`)
+VALUES ('teste auto increment','nao','funciona aaaa ele deixa colocar','')
+
+UPDATE `usuario` SET `funcao`='Presiente' WHERE id = 11
