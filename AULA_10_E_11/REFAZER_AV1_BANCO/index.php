6a2c868a69943fc125dcc721e3c3b6d6e201b39d
beriba-faeterj <104885703+beriba-faeterj@users.noreply.github.com>
2022-06-29 08:18:41 -0300

    Update index.php

--- AULA_10_E_11/REFAZER_AV1_BANCO/index.php
+++ AULA_10_E_11/REFAZER_AV1_BANCO/index.php
@@ -25,7 +25,7 @@ Aulas 10 e 11 - 02/06/2022 e 09/06/2022 - Refazer AV1 com banco de dados
     </nav>
     <main>
         <h1>Aulas 10 e 11 - Beriba - 3DAW Manhã - 2022.1</h1>
-        <h2>Refazer AV1 com banco de dados (ebloga exercícios 24, 25...)</h2>
+        <h2>Refazer AV1 com banco de dados (engloba exercícios 24, 25...)</h2>
         <h3>Observações</h3>
         <ul>
             <li>Aparentemente o PHP não lida muito bem com requisições DELETE sozinho (não puxa variáveis direto que nem o POST e o GET), então usei GET para listar e POST para todos os outros (referência: https://www.php.net/manual/pt_BR/reserved.variables.php)</li>
